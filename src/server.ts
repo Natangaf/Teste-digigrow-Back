c9992bef36bfa198a2b864f1d5b16ba65ae1fb5c
Natã fernandes <natanfernandes52@gmail.com>
2024-08-12 16:36:46 -0300

    re

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import connectToDatabase from "./database";
 const startServer = async () => {
   try {
     await connectToDatabase();
-    app.listen(3000, () => {
+    const PORT = process.env.PORT || 3000; 
+    app.listen(PORT, () => {
       console.log("Servidor executando na porta 3000");
     });
   } catch (error) {
